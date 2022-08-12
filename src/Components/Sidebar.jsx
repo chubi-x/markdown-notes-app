c1170456d8624061846f81c89bde28a6194ae109
chubi-x <chubixax@protonmail.com>
2022-08-12 14:38:50 +0100

    feat: complete sidebar styling

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,4 +1,16 @@
 import React from "react";
 export default function Sidebar() {
-  return <h1>This is the sidebar</h1>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-header-container">
+        <div className="sidebar-header">
+          <h1>Notes</h1>
+        </div>
+        <div className="new-note">
+          <button> + </button>
+        </div>
+      </div>
+      <div className="note"></div>
+    </div>
+  );
 }
